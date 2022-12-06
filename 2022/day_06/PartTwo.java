ed8be305c47e5a0d313ef440f4d262847cf32aab
Håkan Lund <howken@howken.net>
2022-12-06 23:48:56 +0100

    Solutions for day 6

---
+++ 2022/day_06/PartTwo.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class PartTwo {
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader;
+        Integer sum = 0;
+
+        try {
+            reader = new BufferedReader(new FileReader("input.txt"));
+            String line = reader.readLine();
+            
+            while (line != null) {
+                Boolean result = true;
+                // check substring of line until check is true
+                for(int i=0;i<line.length()-13;i++) {
+                    String subString = line.substring(i, (i+14));
+                    //System.out.println("Substring: "+subString);
+                    HashSet <Character> uniqueCharSet = new HashSet();
+                    Boolean foundNoMatch = true;
+                    for(int j=0; j < subString.length() ; j++) {
+                        result=uniqueCharSet.add(subString.charAt(j));
+                        if (result==false) {
+                            //System.out.println("String: "+subString);
+                            foundNoMatch = false;
+                            break;
+                        }
+                    }
+                    if (foundNoMatch) {
+                        System.out.println("right String: "+subString);
+                        System.out.println("Start char at: "+(i+14));
+                        break;
+                    }
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Result:  " + Integer.toString(sum));
+    }
+}
